37a37d53860a4d12c1821313a7b1223832fcba60
Yann Pham-Van <141633300+YannPhamVan@users.noreply.github.com>
2025-05-21 21:47:01 +0000

    Add HW2

--- cohorts/2025/02-experiment-tracking/homework/train.py
+++ cohorts/2025/02-experiment-tracking/homework/train.py
@@ -3,7 +3,9 @@ import pickle
 import click
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
+
+import mlflow
 
 
 def load_pickle(filename: str):
@@ -19,14 +21,18 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    mlflow.autolog()
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    with mlflow.start_run():
+        
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = root_mean_squared_error(y_val, y_pred)
 
 
 if __name__ == '__main__':
